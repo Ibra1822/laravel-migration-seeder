067fa82fcbb38402e93f79101ff44366aac12810
“Ibra1822” <ibrahimmujagic18@gmail.com>
2023-01-10 20:29:09 +0100

    add all

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,21 @@
 @extends('layout.main')
 
 @section('content')
-<h1>home</h1>
+<h1>Partenze Treni</h1>
+
+<div class="cont-card">
+@foreach ($trains as $train)
+    <div class="card">
+      <div>
+        <h2> Treno: {{$train->azienda}}</h2>
+        <h2>Parte da: {{$train->stazione_partenza}}</h2>
+        <h2>Arriva a: {{$train->stazione_arrivo}}</h2>
+        <h2>Parte alle: {{$train->orario_partenza}}</h2>
+        <h2>Arriva alle: {{$train->orario_arrivo}}</h2>
+       </div>
+    </div>
+    @endforeach
+
+</div>
+
 @endsection
